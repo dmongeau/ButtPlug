e7f466889123e91d827bc9b618d464d6ead257c2
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-05-13 12:39:04 -0400

    Added comments

--- example.php
+++ example.php
@@ -2,16 +2,16 @@
 
 class Service {
 	
-	public $secret = 'CLE_SECRETE';
-	public $debug = true;
-	public $listMethods = false;
+	public $secret = 'CLE_SECRETE';	//Secret key
+	public $debug = true; 			//Debug mode, the signature is not validated
+	public $listMethods = true; 	//Allow the listMethods method
 	
 	public function get($data) {
 		
 		return $data;
 	}
 	
-	public function error($data) {
+	public function update($data) {
 		
 		return $data;
 	}
